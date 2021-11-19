28b9eecae9c5ba45d1099a4639746071427c70f3
unknown <rakibulhasan.seu.cse@gmail.com>
2021-11-19 23:52:33 +0600

    done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCo7G9NWaE3IvrjNAydiRj-8W2RPb7ml1A",
-  authDomain: "car-leader.firebaseapp.com",
-  projectId: "car-leader",
-  storageBucket: "car-leader.appspot.com",
-  messagingSenderId: "429440973871",
-  appId: "1:429440973871:web:5099e61fc1eaf1828f5ada",
-  measurementId: "G-0HCNMNEB6B"
+  apiKey: "AIzaSyCmsqTWXebpinwlsEEU9L1Plpy3vouXLgw",
+  authDomain: "carleader-378c6.firebaseapp.com",
+  projectId: "carleader-378c6",
+  storageBucket: "carleader-378c6.appspot.com",
+  messagingSenderId: "618476239568",
+  appId: "1:618476239568:web:df7f090ae6acebdf07fcd5"
 };
 
 export  default firebaseConfig;
